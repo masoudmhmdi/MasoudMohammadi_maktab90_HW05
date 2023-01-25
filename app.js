55d905d7b7c82f807aac1811f82033bbfcc6c3a4
msdmhmdi <masoudmhmdi86@gmail.com>
2023-01-25 17:59:27 +0330

    practis5 is done

--- app.js
+++ app.js
@@ -1,1 +1,23 @@
-"use strict"
+"use strict";
+// function ask(question, yes, no) {
+//   if (confirm(question)) yes();
+//   else no();
+// }
+// ask(
+//   "Do you agree?",
+//   function () {
+//     alert("You agreed.");
+//   },
+//   function () {
+//     alert("You canceled the execution.");
+//   }
+// );
+
+
+const ask =(question,yes,no)=>{
+    if(confirm(question)) yes()
+    else no()
+}
+
+ask("Do you Agre?",()=>alert("you agreed"),()=>alert("you cancel the execution"))
+
