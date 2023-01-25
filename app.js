790546672b28cd200e04e06bc8e92b84ceb33215
msdmhmdi <masoudmhmdi86@gmail.com>
2023-01-25 12:24:28 +0330

    practis2 is done

--- app.js
+++ app.js
@@ -1,1 +1,25 @@
 "use strict"
+
+let stepCounter ={
+    spep_count:0,
+    increase:function(){
+        this.spep_count+=1
+        return this 
+    },
+    decrease(){
+        this.spep_count-=1
+        return this
+    },
+    reset(){
+        this.spep_count=0
+        return this
+    },
+    read(){
+        console.log(this.spep_count)
+        return this
+    }
+    
+
+}
+
+stepCounter.increase().increase().increase().read().decrease().decrease().decrease().increase().read().reset().read()
