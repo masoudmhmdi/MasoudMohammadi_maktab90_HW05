a8da9309e3ff981868280701b55f795e40b5cf2f
msdmhmdi <masoudmhmdi86@gmail.com>
2023-01-25 15:40:47 +0330

    practis4 is done

--- app.js
+++ app.js
@@ -1,1 +1,18 @@
-"use strict"
+"use strict";
+const alarm = document.getElementById("alarm");
+const btn = document.getElementById("btn");
+
+function setAlarm() {
+  let top = `${prompt("top?")}px`;
+  let left = `${prompt("left?")}px`;
+
+  if (top !== "nullpx" && left !== "nullpx" && top !== "px" && left !== "px") {
+    alarm.style.top = top;
+    alarm.style.left = left;
+    alarm.firstChild.textContent = `${prompt("Text?")}`;
+    alarm.style.height = "70px";
+    alarm.style.width = "200px";
+  }
+}
+
+btn.addEventListener("click", setAlarm);
