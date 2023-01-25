1091a34d2df77a6b4699da59d2161b09f588063c
msdmhmdi <masoudmhmdi86@gmail.com>
2023-01-25 13:16:51 +0330

    practise3 is done

--- app.js
+++ app.js
@@ -1,1 +1,24 @@
-"use strict"
+"use strict";
+let myData = [];
+
+while (true) {
+  let name = prompt("what is your name?");
+  if (name) name.split(" ").join("");
+
+  let age = prompt("How old are you?");
+  if (age) age.split(" ").join("");
+
+  if (name === null || name === "" || age === null || age === "") {
+    myData.sort((a, b) => a.age - b.age);
+    console.log(myData);
+  }
+
+  if (name === null || name === "" || age === null || age === "") break;
+
+  let localObj = {
+    name,
+    age: Number(age),
+  };
+  myData.push(localObj);
+  console.log(myData);
+}
